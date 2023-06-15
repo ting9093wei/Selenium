9267881228ec7d3d82860e39515ac2d1430f2828
ting9093wei <70374091+ting9093wei@users.noreply.github.com>
2023-06-15 15:56:49 +0800

    message

--- selenium_test.py
+++ selenium_test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://ithelp.ithome.com.tw/users/20124633/articles')
+driver.get('https://hackmd.io/@arotioppie')
 driver.close()
